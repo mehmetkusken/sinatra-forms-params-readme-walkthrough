f6f25421bebb073194bc38e795c531cfe14f17dc
Mehmet <mehmetkusken@gmail.com>
2021-05-03 23:59:17 -0600

    commit tree 9bb0c690166cc954472ec978dbf3fe8adc230f91

--- app.rb
+++ app.rb
@@ -1,11 +1,16 @@
 require_relative 'config/environment'
-
 class App < Sinatra::Base
-
   get '/food_form' do
     erb :food_form
   end
 
+
   # Add your post route and action below
+  post '/food' do
+    name = params[:name]
+    food = params[:favorite_food]
+    "My name is #{name}, and I love #{food}."
+  end
+
 
 end
